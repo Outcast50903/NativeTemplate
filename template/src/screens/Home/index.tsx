1cd9b47838825c726ab7ea4db525e32b789d132a
Diego Garcia <dgarciavallejos@gmail.com>
2023-09-23 22:22:45 -0300

    Update Home Screen

--- template/src/screens/Home/index.tsx
+++ template/src/screens/Home/index.tsx
@@ -1,49 +1,29 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import useQueryFact from 'api/hooks/Fact';
-import { Button, Card, Container } from 'components';
-import { BottomTabStackParamList } from 'navigation';
+import { useActionsAuth } from 'common';
+import { factSelectedAtom } from 'common/atoms';
 import { useAtomValue } from 'jotai';
-import { factSelectedAtom } from 'atoms';
-import { Text, View } from 'tamagui';
+import { BottomTabStackParamList } from 'navigation';
+import { Button, Card,Text, View } from 'tamagui';
+
+import { Container } from 'components';
 
 const HomeScreen = () => {
   const navigation = useNavigation<StackNavigationProp<BottomTabStackParamList>>();
-  const fact = useAtomValue(factSelectedAtom);
-  const {factQuery} = useQueryFact();
+  const { dispatchLogout } = useActionsAuth();
+  // const fact = useAtomValue(factSelectedAtom);
 
   return (
     <Container>
-      <View className="text-justify flex-1">
-        <Text color='black' className="text-2xl text-center mb-2 dark:text-white">
-          React-Native Template
-        </Text>
+      <View flex={1}>
         <Card>
           <Text color='black' className="text-lg">
             This is a template for React-Native projects using TypeScript,
             TailwindCSS, and React-Navigation.
           </Text>
-          <Text color='black' className="mt-3 text-lg">
-            It also includes TanStack Query for handle API calls. See more
-            details of the libraries here
-          </Text>
-          <Button
-            onPress={() => navigation.navigate('DETAILS')}
-            text="Go to Details"
-            custom={{
-              text: 'text-white',
-              background: 'bg-blue-500',
-              activeBackground: 'bg-blue-700',
-            }}
-          />
-          <Text testID='api-fact-id' className="text-slate-900 text-sm font-thin text-center mt-3">
-            {
-              factQuery.isFetching || factQuery.isError
-                ? 'Loading...'
-                : fact
-            }
-          </Text>
+          <Button onPress={() => navigation.navigate('DETAILS')}>Go to Details</Button>
+          <Button onPress={() => dispatchLogout()}>Cerrar Sesión</Button>
         </Card>
       </View>
     </Container>
