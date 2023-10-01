b8a3db99002cedd2959558883b4bb96a20d18ad1
Diego Garcia <dgarciavallejos@gmail.com>
2023-10-01 12:03:14 -0300

    update store

--- template/src/common/store/auth/index.ts
+++ template/src/common/store/auth/index.ts
@@ -2,67 +2,39 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { AuthDecoded, AuthStoreState } from 'common/types'
 import jwt_decode from 'jwt-decode'
 import { create } from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 export const useAuthStore = create(
-  devtools(
-    persist<AuthStoreState>(
-      (set) => ({
-        userId: null,
-        accessToken: null,
-        refreshToken: null,
-        isAuth: false,
-        actions: {
-          dispatchLogout: async () => {
-            await AsyncStorage.removeItem('authStore')
-            set({
-              userId: null,
-              accessToken: null,
-              refreshToken: null,
-              isAuth: false,
-            })
-          },
-          dispatchLogin: (accessToken: string, refreshToken: string) => {
-            const token: AuthDecoded = jwt_decode(accessToken)
-            
-            console.tron.log({
-              accessToken,
-              refreshToken,
-              isAuth: true,
-              userId: token.id,
-            })
-
-            set({
-              accessToken,
-              refreshToken,
-              isAuth: true,
-              userId: token.id,
-            })
-          },
+  persist<AuthStoreState>(
+    (set) => ({
+      userId: null,
+      accessToken: null,
+      refreshToken: null,
+      isAuth: false,
+      actions: {
+        dispatchLogout: async () => {
+          await AsyncStorage.removeItem('authStore')
+          set(() => ({
+            userId: null,
+            accessToken: null,
+            refreshToken: null,
+            isAuth: false,
+          }))
         },
-      }), {
-      name: 'authStore',
-      storage: {
-        getItem: async name => {
-          const str = await AsyncStorage.getItem(name) || '{}'
-
-          return {
-            ...JSON.parse(str),
-          }
-        },
-        setItem: (name, newValue) => {
-          const str = JSON.stringify({
-            ...newValue.state,
+        dispatchLogin: (accessToken: string, refreshToken: string) => {
+          const token: AuthDecoded = jwt_decode(accessToken)
+          set({
+            accessToken,
+            refreshToken,
+            isAuth: true,
+            userId: token.id,
           })
-
-          AsyncStorage.setItem(name, str)
         },
-        removeItem: name => AsyncStorage.removeItem(name),
       },
-      partialize: state => (state),
+    }), {
+      name: 'authStore',
+      storage: createJSONStorage(() => AsyncStorage),
     },
-    ),
-    { enabled: false },
   ),
 )
 
