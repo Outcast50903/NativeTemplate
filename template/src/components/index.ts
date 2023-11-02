67799f2829407002e3301eee993f29a859589e64
Diego Garcia <dgarciavallejos@gmail.com>
2023-11-02 17:37:20 -0300

    update components

--- template/src/components/index.ts
+++ template/src/components/index.ts
@@ -1,7 +1,4 @@
-export {default as Button} from './Button';
-export {default as Card} from './Card';
 export {default as Container} from './Container';
-export {default as List} from './List';
 export {default as Loading} from './Loading';
 export * from './Svg';
 export * from './Svg/types';
