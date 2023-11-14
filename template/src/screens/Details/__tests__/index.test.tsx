2e687cc86f558066895faf544fc40d8b6a863573
Diego Garcia <dgarciavallejos@gmail.com>
2023-11-13 21:36:11 -0300

    Add snapshot test and update render tests in DetailsScreen

--- template/src/screens/Details/__tests__/index.test.tsx
+++ template/src/screens/Details/__tests__/index.test.tsx
@@ -1,51 +1,82 @@
 import React from 'react';
 import {describe, expect,it} from '@jest/globals';
-import {render} from '@testing-library/react-native';
+import {render, screen} from '@testing-library/react-native';
 
 import data from '../data';
 import DetailsScreen from '..';
 
 describe('DetailsScreen', () => {
-  it('should render Container and Card components', () => {
-    const wrapper = render(<DetailsScreen />);
+  it('should render generate snapshot', () => {
+    render(<DetailsScreen />);
+    
+    expect(screen).toMatchSnapshot();
+  });
 
-    expect(wrapper.findByTestId('custom-container-id')).toBeDefined();
-    expect(wrapper.findByTestId('custom-card-id')).toBeDefined();
+  it('should render Container and Card components', () => {
+    render(<DetailsScreen />);
+    
+    expect(screen.findByTestId('custom-container-id')).toBeDefined();
+    expect(screen.findByTestId('custom-card-id')).toBeDefined();
   });
 
   it('should render Text components with correct text', () => {
-    const {getByText} = render(<DetailsScreen />);
+    render(<DetailsScreen />);
 
-    expect(getByText('Dependencies')).toBeTruthy();
-    expect(getByText('This project uses the following dependencies:')).toBeTruthy();
-    expect(getByText('\u2022 React Navigation')).toBeTruthy();
-    expect(getByText('\u2022 Axios')).toBeTruthy();
-    expect(getByText('\u2022 Jotai')).toBeTruthy();
-    expect(getByText('\u2022 Nativewind')).toBeTruthy();
-    expect(getByText('\u2022 TanStackQuery (React Query)')).toBeTruthy();
-    expect(getByText('\u2022 Dayjs')).toBeTruthy();
-    expect(getByText('\u2022 React native chart kit')).toBeTruthy();
-    expect(getByText('\u2022 React native SVG')).toBeTruthy();
+    expect(screen.getByText('Dependencies')).toBeTruthy();
+    expect(screen.getByText('This project uses the following dependencies:')).toBeTruthy();
+    expect(screen.getByText('\u2022 React Navigation')).toBeTruthy();
+    expect(screen.getByText('\u2022 Axios')).toBeTruthy();
+    expect(screen.getByText('\u2022 Jotai')).toBeTruthy();
+    expect(screen.getByText('\u2022 Nativewind')).toBeTruthy();
+    expect(screen.getByText('\u2022 TanStackQuery (React Query)')).toBeTruthy();
+    expect(screen.getByText('\u2022 Dayjs')).toBeTruthy();
+    expect(screen.getByText('\u2022 React native chart kit')).toBeTruthy();
+    expect(screen.getByText('\u2022 React native SVG')).toBeTruthy();
   });
 
-  it('should render FlatList component with correct data and renderItem prop', () => {
-    const {getByTestId,getAllByTestId} = render(<DetailsScreen />);
-    
-    expect(getByTestId('details-list-id')).toBeDefined();
-    expect(getByTestId('details-list-id').props.data).toEqual(data);
-    expect(getAllByTestId('details-item-component-id')).toBeDefined();
-    expect(getAllByTestId('details-item-component-id').length).toEqual(data.length);
+  it('should render FlatList component with correct data and renderItem prop', () => {    
+    render(<DetailsScreen />);
+
+    expect(screen.getByTestId('details-list-id')).toBeDefined();
+    expect(screen.getByTestId('details-list-id').props.data).toEqual(data);
+    expect(screen.getAllByTestId('details-item-component-id')).toBeDefined();
+    expect(screen.getAllByTestId('details-item-component-id').length).toEqual(data.length);
   });
 
   it('should render Container component with the correct props', () => {
-    const {getByTestId} = render(<DetailsScreen />);
+    render(<DetailsScreen />);
 
-    expect(getByTestId('custom-container-id').props.children).toBeDefined();
+    expect(screen.getByTestId('custom-container-id').props.children).toBeDefined();
   });
 
   it('should render Card component with the correct props', () => {
-    const {getByTestId} = render(<DetailsScreen />);
+    render(<DetailsScreen />);
 
-    expect(getByTestId('custom-card-id').props.children).toBeDefined();
+    expect(screen.getByTestId('custom-card-id').props.children).toBeDefined();
+  });
+
+  it('should render Text component with the correct props', () => {
+    render(<DetailsScreen />);    
+
+    expect(screen.getByTestId('custom-card-id')).toHaveStyle({ backgroundColor: 'red' });
+  });
+
+  it(`should render Text 'Dependencies' with color style black`, () => {
+    render(<DetailsScreen />);
+    
+    expect(screen.getByText('Dependencies')).toHaveStyle({ color: 'black', fontSize: 24, lineHeight: 32 });
+  });
+
+  it(`should render Text 'This project uses the following dependencies:' with color style black`, () => {
+    render(<DetailsScreen />);
+    
+    expect(screen.getByText('This project uses the following dependencies:'))
+      .toHaveStyle({ color: 'black', fontSize: 16, lineHeight: 24 });
+  })
+
+  it(`should count data lenght`, () => {
+    render(<DetailsScreen />);
+    
+    expect(data.length).toEqual(8);
   });
 });
