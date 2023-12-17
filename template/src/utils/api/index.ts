759fd26934e938998c16ce9c2478b1c5b090ff9e
Diego Garcia <dgarciavallejos@gmail.com>
2023-12-17 18:59:30 -0300

    Refactor API client methods

--- template/src/utils/api/index.ts
+++ template/src/utils/api/index.ts
@@ -15,71 +15,43 @@ export default class AxiosHttpClient extends HttpClient {
         ...headers,
       },
     });
-
-    this.api.interceptors.request.use(config => {
-      headers?.Authenticate
-        ? (config.headers.Authenticate =
-            headers?.Authenticate || headers?.Authorization || '')
-        : (config.headers.Authorization =
-            headers?.Authenticate || headers?.Authorization || '');
-
-      return config;
-    });
   }
 
   async get<TResponse>(url: string): Promise<TResponse> {
-    try {
-      const {data} = await this.api.get<TResponse>(url);
-      return data;
-    } catch (error) {
-      return null as TResponse;
-    }
+    const { data } = await this.api.get<TResponse>(url);
+    return data;
   }
 
   async post<TResponse, UBody>(
     url: string,
     body: UBody,
   ): Promise<TResponse> {
-    try {
-      const {data} = await this.api.post<TResponse>(url, body);
-      return data;
-    } catch (error) {
-      return null as TResponse;
-    }
+    const {data} = await this.api.post<TResponse>(url, body);
+    console.log('data', data);
+    
+    return data;
   }
 
   async put<TResponse, UBody>(
     url: string,
     body: UBody,
   ): Promise<TResponse> {
-    try {
-      const {data} = await this.api.put<TResponse>(url, body);
-      return data;
-    } catch (error) {
-      return null as TResponse;
-    }
+    const {data} = await this.api.put<TResponse>(url, body);
+    return data;
   }
 
   async patch<TResponse, UBody>(
     url: string,
     body: UBody,
   ): Promise<TResponse> {
-    try {
-      const {data} = await this.api.patch<TResponse>(url, body);
-      return data;
-    } catch (error) {
-      return null as TResponse;
-    }
+    const {data} = await this.api.patch<TResponse>(url, body);
+    return data;
   }
 
   async delete<TResponse>(
     url: string,
   ): Promise<TResponse> {
-    try {
-      const {data} = await this.api.delete<TResponse>(url);
-      return data;
-    } catch (error) {
-      return null as TResponse;
-    }
+    const {data} = await this.api.delete<TResponse>(url);
+    return data;
   }
 }
